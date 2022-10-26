8e0c8e72bb76e254ee92313fefd6f7c5d5694a98
Vinh Phu <19522020@gm.uit.edu.vn>
2022-10-26 22:15:35 +0700

    test query by setup file

--- sqlite-run.mjs
+++ sqlite-run.mjs
@@ -1,11 +1,27 @@
 import {open} from 'sqlite';
 import sqlite3 from 'sqlite3';
+import {unlinkSync, existsSync} from 'fs';
 
 async function setup(){
-	const db = await open({filename:'./mydb.sqlite',driver:sqlite3.Database});
+	const db_file_name = './database.sqlite';
+
+	//if database file exists
+	if (await existsSync(db_file_name)) 
+	{
+		//remove the database file
+		await unlinkSync(db_file_name);
+	}
+
+	//open and connect to the database
+	const db = await open({filename:db_file_name,driver:sqlite3.Database});
 	await db.migrate({force:true})
 
 	console.log("Database ready");
+
+	//for test
+	const res = await db.all('SELECT * FROM ROLE');
+	console.log(res);
+
 }
 
 setup();
