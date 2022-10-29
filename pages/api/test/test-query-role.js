49d830180f9689a335da0437771a4075d22d0e1a
Vinh Phu <19522020@gm.uit.edu.vn>
2022-10-29 10:26:47 +0700

    add signup api file

--- pages/api/test/test-query-role.js
+++ pages/api/test/test-query-role.js
@@ -9,10 +9,7 @@ export default async function handler(req, res) {
 	const db = await open({filename:'./database.sqlite',driver:sqlite3.Database});
 
 	const query_result = await db.all('SELECT * FROM ROLE');
-	// parse query result to json
-	const json_result = JSON.stringify(query_result);
+	// res.status(200).json(pretty_json_result);
 
-	// console.log(json_result);
-	// send json result to client
-	res.status(200).json(json_result);
+	res.status(200).json(query_result);
 }
