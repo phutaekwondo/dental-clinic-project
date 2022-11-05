2ba74ef4e3eecd82621aea48aec83e7bd660fe2f
Vinh Phu <19522020@gm.uit.edu.vn>
2022-11-05 13:32:16 +0700

    add get-all-appointments api

--- pages/api/appointment/get-all-appointments.ts
+++ pages/api/appointment/get-all-appointments.ts
@@ -1,7 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 // import api resquest response from next
 import {NextApiRequest, NextApiResponse} from 'next';
-import { CheckFields } from '../../../helpers/request/request-helper';
+import Appointment from '../../../classes/appointment.mjs';
 
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+	try{
+		const result = await Appointment.GetAllAppointments();
+		return res.status(200).json(result);
+	}	
+	catch{
+		return res.status(400).json({message: 'Error getting appointments'});
+	}
 }
