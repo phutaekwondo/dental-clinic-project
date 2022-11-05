07dfb7b9eab8cfdcd62e42f4a8cf3a947f32b5d4
Vinh Phu <19522020@gm.uit.edu.vn>
2022-11-05 13:10:25 +0700

    add files, remove non-use included in signup

---
+++ pages/api/appointment/get-all-appointments.ts
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import api resquest response from next
+import {NextApiRequest, NextApiResponse} from 'next';
+import { CheckFields } from '../../../helpers/request/request-helper';
+
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+}
