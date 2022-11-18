47afae1b59f3c5f79816c3ca92accc4490cd97f4
tr-nhan <20520666@gm.uit.edu.vn>
2022-11-18 15:19:52 +0700

    fix issues contacting databases

--- pages/api/patient_list.js
+++ pages/api/patient_list.js
@@ -12,28 +12,41 @@ export default async function handler(req, res) {
             return res.status(403).json({message: "FAIL"});
         }
     });
-
-    let queryResult = await retrieveData(db);
+    // Bắt lỗi khi query
+    try {
+        let queryResult = await retrieveData(db);
+        return res.status(200).json(queryResult);
+    } catch {
+        return res.status(403).json({message: "FAIL"});
+    } finally {
+        db.close((err) => {
+            if (err) throw err;
+        });
+    }
     // Ngắt kết nối với database
-    db.close((err) => {if(err) throw err;});
-    return res.status(200).json(queryResult);
+    db.close((err) => {
+        if (err) throw err;
+    });
+    // Nếu thành công thì chương trình sẽ không thể thực thi tới đây do đã return trong khối try catch
+    // Nếu tới đây thì nghĩa đoạn code bị lỗi -> return res status 500 internal error
+    return res.status(500).json({message: "FAIL"}).end();
 }
-function retrieveData(db)
-{
-    return new Promise((resolve)=>{
-        let queryResult = [];
+
+function retrieveData(db) {
+    return new Promise((resolve, reject) => {
+        let data = [];
         // Query với cú pháp của sqlite3 (MySQL)
         // ID, tên bệnh nhân, giới tính, ngày sinh, email
         let sql = "SELECT p_id, p_name, p_sex, p_dateOB, p_email FROM PATIENT";
         db.all(sql, (err, rows) => {
             if (err) {
-                throw err;
+                reject(err);
             }
             // Mỗi element trong array queryResult sẽ chứa 1 JSON về 1 bệnh nhân
             rows.forEach((row) => {
-                queryResult.push(row);
+                data.push(row);
             });
-            resolve(queryResult);
+            resolve(data);
         });
     });
 }
