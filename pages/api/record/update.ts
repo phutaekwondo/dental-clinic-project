417ae81d17ccb24c17fc321995104c1e69260c91
Quang Huy <phamquanghuy2809@gmail.com>
2022-11-26 19:06:44 +0700

    update

--- pages/api/record/update.ts
+++ pages/api/record/update.ts
@@ -5,59 +5,63 @@ export default async function handleUpdateRecord(
     res: NextApiResponse
 ) {
     //check fields
-    const db = await GetDatabase();
     let {
-        patient_id,
+        rec_id,
         rec_dease,
         rec_desc,
         rec_indiagnose,
         rec_outdiagnose,
         rec_conclusion,
     } = req.body;
-    if (
-        !patient_id ||
-        !rec_dease ||
-        !rec_desc ||
-        !rec_indiagnose ||
-        !rec_outdiagnose ||
-        !rec_conclusion
-    ) {
-        return res.status(200).json({
-            errCode: 1,
-            errMessage: "Missing parameter!",
-        });
-    } else {
-        let appointment = await db.get(
-            `SELECT * FROM APPOINTMENT WHERE p_id=${patient_id}`
-        );
-        if (appointment) {
-            let record = {};
-            record = await db.get(
-                `SELECT * FROM RECORD WHERE appoint_id=${appointment.appoint_id}`
-            );
-            if (record) {
-                await db.run(`
-                UPDATE RECORD 
-                SET rec_dease=${rec_dease},
-                rec_desc=${rec_desc},
-                rec_indiagnose=${rec_indiagnose},
-                rec_outdiagnose=${rec_outdiagnose},
-                rec_conclusion=${rec_conclusion} 
-                WHERE appoint_id = ${appointment.appoint_id}`);
-            }
-            record = await db.get(
-                `SELECT * FROM RECORD WHERE appoint_id=${appointment.appoint_id}`
-            );
+    const db = await GetDatabase();
+    try {
+        //Kiem tra req.body
+        if (
+            !rec_id ||
+            !rec_dease ||
+            !rec_desc ||
+            !rec_indiagnose ||
+            !rec_outdiagnose ||
+            !rec_conclusion
+        ) {
             return res.status(200).json({
-                errCode: 0,
-                errMessage: "OK",
-                record,
-            });
-        } else {
-            return res.status(200).json({
-                errCode: 2,
-                errMessage: "Not found this patient!",
+                errCode: 1,
+                errMessage: "Missing parameter!",
             });
         }
+        // Tim benh an
+        else {
+            let record = {};
+            record = await db.get(
+                `SELECT * FROM RECORD WHERE rec_id=${rec_id}`
+            );
+            //Neu co thi sua
+            if (record) {
+                await db.run(`
+                    UPDATE RECORD 
+                    SET rec_dease=${rec_dease},
+                    rec_desc=${rec_desc},
+                    rec_indiagnose=${rec_indiagnose},
+                    rec_outdiagnose=${rec_outdiagnose},
+                    rec_conclusion=${rec_conclusion} 
+                    WHERE rec_id = ${rec_id}`);
+                let result = await db.get(
+                    `SELECT * FROM RECORD WHERE rec_id=${rec_id}`
+                );
+                return res.status(200).json({
+                    errCode: 0,
+                    errMessage: "OK",
+                    update: result,
+                });
+            } else {
+                //Neu khong co
+                return res.status(200).json({
+                    errCode: 2,
+                    errMessage: "This record doesn't exist!",
+                });
+            }
+        }
+    } catch (e) {
+        console.log(">>ERROR: ", e);
     }
 }
